f1ae2a1c1f4f25e06fd320bfc2e9a04eccf10ef5
JiaJia <2894220@gmail.com>
2016-07-30 18:44:41 +0800

    [HookNtCreateUserProcess] Hook NtCreateUserProcess function
     - 实现文件创建和修改监控的区分。
     - 实现 Win7x64 的进程监控及文件监控。输出带有进程 PID、参数等信息

--- ExampleDll/dllmain.cpp
+++ ExampleDll/dllmain.cpp
@@ -2,11 +2,12 @@
 #include "stdafx.h"
 #include "easyhook.h"
 #include "DriverShared.h"
+#include "NtStructDef.h"
 
 EASYHOOK_BOOL_EXPORT EasyHookDllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved);
 
 // NtCreateFile 的函数指针
-typedef NTSTATUS(NTAPI* pfnNTCREATEFILE) (
+typedef NTSTATUS (NTAPI* pfnNTCREATEFILE) (
 	OUT PHANDLE             FileHandle,
 	IN ACCESS_MASK          DesiredAccess,
 	IN POBJECT_ATTRIBUTES   ObjectAttributes,
@@ -18,12 +19,32 @@ typedef NTSTATUS(NTAPI* pfnNTCREATEFILE) (
 	IN ULONG                CreateOptions,
 	IN PVOID                EaBuffer OPTIONAL,
 	IN ULONG                EaLength
-	);
+);
+
+typedef NTSTATUS (NTAPI* pfnNTCREATEUSERPROCESS) (
+	OUT PHANDLE ProcessHandle,
+	OUT PHANDLE ThreadHandle,
+	IN ACCESS_MASK ProcessDesiredAccess,
+	IN ACCESS_MASK ThreadDesiredAccess,
+	IN POBJECT_ATTRIBUTES ProcessObjectAttributes OPTIONAL,
+	IN POBJECT_ATTRIBUTES ThreadObjectAttributes OPTIONAL,
+	IN ULONG CreateProcessFlags,
+	IN ULONG CreateThreadFlags,
+	IN PRTL_USER_PROCESS_PARAMETERS ProcessParameters,
+	IN PVOID Parameter,
+	IN PNT_PROC_THREAD_ATTRIBUTE_LIST AttributeList
+);
 
 // 全局变量
-pfnNTCREATEFILE			pfnNtCreateFile = (pfnNTCREATEFILE)GetProcAddress(GetModuleHandle(_T("ntdll.dll")), "NtCreateFile");
+pfnNTCREATEFILE			pfnNtCreateFile = NULL;
+pfnNTCREATEUSERPROCESS	pfnNtCreateUserProcess = NULL;
 TRACED_HOOK_HANDLE      hHookNtCreateFile = new HOOK_TRACE_INFO();
+TRACED_HOOK_HANDLE      hHookNtCreateUserProcess = new HOOK_TRACE_INFO();
 ULONG                   HookNtCreateFile_ACLEntries[1] = { 0 };
+ULONG                   HookNtCreateUserProcess_ACLEntries[1] = { 0 };
+
+TCHAR					szCurrentProcessName[MAX_PATH] = { 0 };
+DWORD					dwCurrentProcessId;
 
 NTSTATUS NTAPI NtCreateFileHook(OUT PHANDLE FileHandle,
 	IN ACCESS_MASK DesiredAccess,
@@ -37,50 +58,125 @@ NTSTATUS NTAPI NtCreateFileHook(OUT PHANDLE FileHandle,
 	IN PVOID EaBuffer OPTIONAL,
 	IN ULONG EaLength)
 {
-	// 打印被打开的文件名参数
-	if (CreateDisposition == FILE_CREATE &&
-		ShareAccess & (FILE_SHARE_READ | FILE_SHARE_WRITE))
-	{
-		CString csOutput;
-		csOutput.Format(_T("ObjectName = %s, FileAttributes = 0x%08X, ShareAccess = 0x%08X, CreateDisposition  = 0x%08X, CreateOptions = 0x%08X\n"),
-			ObjectAttributes->ObjectName->Buffer,
-			FileAttributes,
-			ShareAccess,
-			CreateDisposition,
-			CreateOptions);
-		OutputDebugString(csOutput);
-	}
+	NTSTATUS ntStatus;
+
 	// 调用系统原有的 NtCreateFile
-	return pfnNtCreateFile(FileHandle, DesiredAccess, ObjectAttributes,
+	ntStatus = pfnNtCreateFile(FileHandle, DesiredAccess, ObjectAttributes,
 		IoStatusBlock, AllocationSize, FileAttributes, ShareAccess,
 		CreateDisposition, CreateOptions, EaBuffer, EaLength);
+
+	CString csOutput;
+
+	if (IoStatusBlock->Information == FILE_CREATED &&		// 文件创建成功
+		CreateDisposition > FILE_CREATE)					// 大于创建文件的参数
+	{
+		csOutput.Format(_T("%s:%ld 创建文件 %s"), szCurrentProcessName, dwCurrentProcessId, ObjectAttributes->ObjectName->Buffer);
+		OutputDebugString(csOutput);
+	}
+	if (IoStatusBlock->Information == FILE_OVERWRITTEN &&	// 文件覆写成功
+		CreateDisposition > FILE_CREATE)					// 大于创建文件的参数
+	{
+		csOutput.Format(_T("%s:%ld 修改文件 %s"), szCurrentProcessName, dwCurrentProcessId, ObjectAttributes->ObjectName->Buffer);
+		OutputDebugString(csOutput);
+	}
+
+	/*csOutput.Format(_T("IoStatusBlock = 0x%08X, ObjectName = %s, FileAttributes = 0x%08X, ShareAccess = 0x%08X, CreateDisposition  = 0x%08X, CreateOptions = 0x%08X\n"),
+		IoStatusBlock->Information,
+		ObjectAttributes->ObjectName->Buffer,
+		FileAttributes,
+		ShareAccess,
+		CreateDisposition,
+		CreateOptions);
+	OutputDebugString(csOutput);*/
+	return ntStatus;
+}
+
+NTSTATUS NTAPI NtCreateUserProcessHook(
+	OUT PHANDLE ProcessHandle,
+	OUT PHANDLE ThreadHandle,
+	IN ACCESS_MASK ProcessDesiredAccess,
+	IN ACCESS_MASK ThreadDesiredAccess,
+	IN POBJECT_ATTRIBUTES ProcessObjectAttributes OPTIONAL,
+	IN POBJECT_ATTRIBUTES ThreadObjectAttributes OPTIONAL,
+	IN ULONG CreateProcessFlags,
+	IN ULONG CreateThreadFlags,
+	IN PRTL_USER_PROCESS_PARAMETERS ProcessParameters,
+	IN PVOID Parameter,
+	IN PNT_PROC_THREAD_ATTRIBUTE_LIST AttributeList
+)
+{
+	NTSTATUS ntStatus;
+	ntStatus = pfnNtCreateUserProcess(ProcessHandle, ThreadHandle, ProcessDesiredAccess,
+		ThreadDesiredAccess, ProcessObjectAttributes, ThreadObjectAttributes,
+		CreateProcessFlags, CreateThreadFlags, ProcessParameters,
+		Parameter, AttributeList);
+
+	// 获取进程PID
+	DWORD dwProcessId = GetProcessId(*ProcessHandle);
+
+	CString csOutput;
+	
+	csOutput.Format(_T("%s:%ld 创建进程 %s:%ld, 参数：%s"), 
+		szCurrentProcessName, dwCurrentProcessId, 
+		ProcessParameters->ImagePathName.Buffer, dwProcessId, 
+		ProcessParameters->CommandLine.Buffer);
+	OutputDebugString(csOutput);
+
+	return ntStatus;
 }
 
 BOOL InstallHook()
 {
-	NTSTATUS	status;
-	TCHAR		szCurrentProcessName[MAX_PATH] = { 0 };
+	NTSTATUS ntStatus;
 
 	GetModuleFileName(NULL, szCurrentProcessName, _countof(szCurrentProcessName));
-	OutputDebugString(szCurrentProcessName);
+	dwCurrentProcessId = GetCurrentProcessId();
 
-	// 开始 Hook NtCreateFile 函数，使其跳转到自己的 NtCreateFileHook 函数中
-	status = LhInstallHook(pfnNtCreateFile, NtCreateFileHook, NULL, hHookNtCreateFile);
-	if (!SUCCEEDED(status))
+	if (NULL != pfnNtCreateFile)
 	{
-		OutputDebugString(_T("LhInstallHook failed..\n"));
-		return FALSE;
-	}
+		// 开始 Hook NtCreateFile 函数，使其跳转到自己的 NtCreateFileHook 函数中
+		ntStatus = LhInstallHook(pfnNtCreateFile, NtCreateFileHook, NULL, hHookNtCreateFile);
+		if (!SUCCEEDED(ntStatus))
+		{
+			OutputDebugString(_T("LhInstallHook failed..\n"));
+			return FALSE;
+		}
 
-	// 开始 Hook，如果不调用这句，Hook 是不生效的
-	status = LhSetExclusiveACL(HookNtCreateFile_ACLEntries, 1, hHookNtCreateFile);
-	if (!SUCCEEDED(status))
+		// 开始 Hook，如果不调用这句，Hook 是不生效的
+		ntStatus = LhSetExclusiveACL(HookNtCreateFile_ACLEntries, 1, hHookNtCreateFile);
+		if (!SUCCEEDED(ntStatus))
+		{
+			OutputDebugString(_T("LhSetInclusiveACL failed..\n"));
+			return FALSE;
+		}
+	}
+	else
 	{
-		OutputDebugString(_T("LhSetInclusiveACL failed..\n"));
-		return FALSE;
+		OutputDebugString(_T("Get pfnNtCreateFile function address is NULL."));
 	}
+	
+	if (NULL != pfnNtCreateUserProcess)
+	{
+		// 开始 Hook NtCreateUserProcess 函数
+		ntStatus = LhInstallHook(pfnNtCreateUserProcess, NtCreateUserProcessHook, NULL, hHookNtCreateUserProcess);
+		if (!SUCCEEDED(ntStatus))
+		{
+			OutputDebugString(_T("LhInstallHook failed..\n"));
+			return FALSE;
+		}
 
-	OutputDebugString(_T("InstallHook success...\n"));
+		// 开始 Hook，如果不调用这句，Hook 是不生效的
+		ntStatus = LhSetExclusiveACL(HookNtCreateUserProcess_ACLEntries, 1, hHookNtCreateUserProcess);
+		if (!SUCCEEDED(ntStatus))
+		{
+			OutputDebugString(_T("LhSetInclusiveACL failed..\n"));
+			return FALSE;
+		}
+	}
+	else
+	{
+		OutputDebugString(_T("Get pfnNtCreateUserProcess function address is NULL."));
+	}
 
 	return TRUE;
 }
@@ -96,9 +192,14 @@ BOOL UnInstallHook()
 		hHookNtCreateFile = NULL;
 	}
 
-	LhWaitForPendingRemovals();
+	if (NULL != hHookNtCreateUserProcess)
+	{
+		LhUninstallHook(hHookNtCreateUserProcess);
+		delete hHookNtCreateUserProcess;
+		hHookNtCreateUserProcess = NULL;
+	}
 
-	OutputDebugString(_T("UninstallHook success...\n"));
+	LhWaitForPendingRemovals();
 
 	return TRUE;
 }
@@ -123,13 +224,16 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 {
 	// 调用 EasyHook 的入口处理函数
 	EasyHookDllMain(hModule, ul_reason_for_call, lpReserved);
+
+	pfnNtCreateFile = (pfnNTCREATEFILE)GetProcAddress(GetModuleHandle(_T("ntdll.dll")), "NtCreateFile");
+	pfnNtCreateUserProcess = (pfnNTCREATEUSERPROCESS)GetProcAddress(GetModuleHandle(_T("ntdll.dll")), "NtCreateUserProcess");
+
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
 	{
 		// 调用 InstallHook 的线程
 		StartHookThread();
-		OutputDebugString(_T("DLL_PROCESS_ATTACH\n"));
 	}
 	break;
 	case DLL_THREAD_ATTACH:
@@ -140,7 +244,6 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	{
 		// 调用 UnInstallHook 的线程
 		UnInstallHook();
-		OutputDebugString(_T("DLL_PROCESS_DETACH\n"));
 	}
 	break;
 	}
